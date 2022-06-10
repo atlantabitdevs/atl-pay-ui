05eebd8604ba891aadcd606d538c61290c2e6625
Georgiy Pyantkovsky <georgiy.pyantkovsky.ukr@gmail.com>
2022-06-10 12:04:59 -0500

    Payment schedule selection

--- src/components/Recurrence.jsx
+++ src/components/Recurrence.jsx
@@ -1,19 +1,30 @@
-import {Button} from "./Button";
+import { Button } from "./Button";
 
 export const Recurrence = (props) => {
-    let classes = "border-solid border-black border-4 p-8 w-xl flex flex-col space-y-8 items-center justify-between";
+    let classes =
+        "border-solid border-black border-4 p-8 w-xl flex flex-col space-y-8 items-center justify-between";
     let disabledClass =
         "recurrence border-solid border-black border-4 p-8 w-xl flex flex-col space-y-8 items-center justify-between disabled";
-    
-    return(
+
+    return (
         <div className={props.disabled ? disabledClass : classes}>
-            <p className="bg-term-gray text-white p-8 rounded-full w-16 h-16 flex items-center justify-center font-bold text-xl">{props.months}</p>
-            
-            <p className="text-4xl text-center">{props.description}</p>
-            
-            <p className="text-2xl text-center">{props.subdescription}</p>
-            
-            <Button>Select</Button>
+            <p className="bg-term-gray text-white p-8 rounded-full w-16 h-16 flex items-center justify-center font-bold text-xl">
+                {props.schedule.months}
+            </p>
+
+            <p className="text-4xl text-center">{props.schedule.description}</p>
+
+            <p className="text-2xl text-center">
+                {props.schedule.subdescription}
+            </p>
+
+            <Button
+                onClick={() =>
+                    props.selectSchedule(props.schedule, props.index)
+                }
+            >
+                Select
+            </Button>
         </div>
-    )
-}
+    );
+};
--- src/screens/PaymentSchedule/PaymentSchedule.jsx
+++ src/screens/PaymentSchedule/PaymentSchedule.jsx
@@ -1,51 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {Button} from "../../components/Button";
+import { Button } from "../../components/Button";
 import { StatusBar } from "../../components/StatusBar";
 import { useStore } from "../../zustand/store";
-import {Recurrence} from "../../components/Recurrence";
-import {ArrowRightIcon} from "@bitcoin-design/bitcoin-icons-react/outline";
+import { Recurrence } from "../../components/Recurrence";
+import { ArrowRightIcon } from "@bitcoin-design/bitcoin-icons-react/outline";
 
 export const PaymentSchedule = () => {
     const store = useStore((state) => state);
     let navigate = useNavigate();
 
-    const DisplaySchedule = () => {
-        let schedule = [
-            {
-                id: 1,
-                months: "1",
-                description: "Pay for my first month",
-                subdescription: "Standard price",
-                disabled: false,
-            },
-            {
-                id: 2,
-                months: "3",
-                description: "Subscribe for 3 months",
-                subdescription: "2% off",
-                disabled: false,
-            },
-            {
-                id: 3,
-                months: "6",
-                description: "Subscribe for 6 months",
-                subdescription: "5% off",
-                disabled: false,
-            },
-        ];
+    let [schedule, setSchedule] = useState([
+        {
+            id: 1,
+            months: "1",
+            description: "Pay for my first month",
+            subdescription: "Standard price",
+            disabled: false,
+        },
+        {
+            id: 2,
+            months: "3",
+            description: "Subscribe for 3 months",
+            subdescription: "2% off",
+            disabled: false,
+        },
+        {
+            id: 3,
+            months: "6",
+            description: "Subscribe for 6 months",
+            subdescription: "5% off",
+            disabled: true,
+        },
+    ]);
 
-        const selectSchedule = (schedule) => {
-            store.setDeal(schedule);
-            console.log("STORE", store);
-        };
-        return schedule.map((time) => (
+    const selectSchedule = (schedule, index) => {
+        console.log("Selected", schedule);
+        console.log("Index", index);
+        setSchedule((previous) => {
+            previous.forEach((schedule, i) => {
+                if (i !== index) {
+                    schedule.disabled = true;
+                } else {
+                    schedule.disabled = false;
+                }
+            });
+            return previous;
+        });
+        store.setPaymentSchedule(schedule);
+    };
+
+    const DisplaySchedule = () => {
+        return schedule.map((time, index) => (
             <Recurrence
-                id={time.id}
-                months={time.months}
-                description={time.description}
-                subdescription={time.subdescription}
+                key={time.id}
+                schedule={time}
                 selectSchedule={selectSchedule}
+                index={index}
                 disabled={time.disabled}
             />
         ));
@@ -55,16 +66,16 @@ export const PaymentSchedule = () => {
             <h2 className="text-4xl text-center">
                 How frequently would you like to pay?
             </h2>
-            
+
             <div className="flex flex-row space-x-8 w-full items-center justify-center">
                 <DisplaySchedule />
             </div>
-            
+
             <Button
                 onClick={() => {
                     navigate("/invoice");
                 }}
-                disabled={!store.schedule}
+                disabled={!store.paymentSchedule}
             >
                 <span>Continue</span>
                 <ArrowRightIcon className="w-8 h-8" />
