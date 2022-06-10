bdc991a17b206dac8b2fd1dcf09fb4fc412b83c1
Georgiy Pyantkovsky <georgiy.pyantkovsky.ukr@gmail.com>
2022-06-09 20:50:29 -0500

    State hooked up for deals

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import styles from "./Card.css";
+import "./Card.css";
 import { Box, Button, Paper } from "@mui/material";
 
-export const Card = () => {
+export const Card = (props) => {
     return (
         <Paper className="Card">
             <Box className="container">
@@ -10,13 +10,15 @@ export const Card = () => {
                     className="card-image"
                     src="https://coworkingmag.com/wp-content/uploads/2021/05/Coworking-space-austin-feature.png"
                 />
-                <p className="card-text">Select Amazing Desk</p>
+                <p className="card-text">{props.deal.text}</p>
+                <p className="card-text">${props.deal.price}</p>
                 <Button
                     variant="contained"
                     color="warning"
                     className="card-btn"
+                    onClick={() => props.selectDeal(props.deal)}
                 >
-                    Deals
+                    Select
                 </Button>
             </Box>
         </Paper>
--- src/screens/Deals/Deals.jsx
+++ src/screens/Deals/Deals.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { Card } from "../../components/index";
 import "./Deals.css";
+import { useStore } from "../../zustand/store";
 
 export const Deals = () => {
     let navigate = useNavigate();
-    let deals = [1, 2, 3];
+    const store = useStore((state) => state);
+
+    useEffect(() => {
+        console.log(store);
+    }, [store]);
+
+    let deals = [
+        { id: 1, text: "Select Amazing Desk", price: "100" },
+        { id: 2, text: "Select Amazing Desk", price: "200" },
+        { id: 3, text: "Select Amazing Desk", price: "300" },
+    ];
+
+    const selectDeal = (deal) => {
+        store.setDeal(deal);
+        console.log("STORE", store);
+    };
 
     const DisplayDeals = () => {
-        return deals.map((id) => <Card />);
+        return deals.map((deal) => (
+            <Card key={deal.id} deal={deal} selectDeal={selectDeal} />
+        ));
     };
 
     return (
@@ -25,7 +43,7 @@ export const Deals = () => {
                 color="warning"
                 className="deals-btn"
             >
-                Deals
+                Done
             </Button>
         </div>
     );
